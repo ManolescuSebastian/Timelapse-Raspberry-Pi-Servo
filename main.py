64db1e3fcd41a74a83d23a67c96166f1161dcd7d
Sebastian <sebastian.manolescu@gmail.com>
2021-04-07 00:14:28 +0300

    DHT11 sensor basic implementation
    Project cleanup

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from flask_restful import Resource, Api
 from camera_pi import Camera
 import RPi.GPIO as GPIO
 import numpy as np
+import board
 from picamera import PiCamera
 import asyncio
-import tty, sys, termios
+import dht11
 
 GPIO.setmode(GPIO.BCM)
 
@@ -18,8 +19,11 @@ api = Api(app)
 servoPIN = 17
 
 GPIO.setup(servoPIN, GPIO.OUT)
-servoVertical = GPIO.PWM(servoPIN, 50)
-servoVertical.start(2.5)
+#servoVertical = GPIO.PWM(servoPIN, 50)
+#servoVertical.start(2.5)
+
+# DTH 11 sensor (temperature and humidity)
+instance = dht11.DHT11(pin = 22)
 
 # Camera timelapse
 @app.route('/timelapse')
@@ -28,15 +32,24 @@ def startTimelapse():
     camera.resolution = (2592, 1944)
     for filename in camera.capture_continuous('timelapse_images/img{counter:03d}.jpg'):
         print('Captured %s' % filename)
-        sleep(15) # wait 15 seconds
+        sleep(5) # wait 5 seconds
     return ""
 
-#default web page
+# Default web page
 @app.route('/')
 def index():
+    result = instance.read()
+
+    if result.is_valid():
+        print("Temperature: %-3.1f C" % result.temperature)
+        print("Humidity: %-3.1f %%" % result.humidity)
+    else:
+        print("Error: %d" % result.error_code)
+    
     return render_template('/index.html')
 
-#display camera video feed using the /stream.mjpg endpoint
+
+# Display camera video feed using the /stream.mjpg endpoint
 def gen(camera):
     #"""Video streaming generator function."""
     while True:
@@ -50,26 +63,6 @@ def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-      
-# min 5 / max 12.5
-verticalMotorValue = 5
-horizontalMotorValue = 5
-
-filedescriptors = termios.tcgetattr(sys.stdin)
-tty.setcbreak(sys.stdin)
-x = 0
-while 1:
-  x=sys.stdin.read(1)[0]
-  if x == "w":
-      print('w pressed')
-      if verticalMotorValue >= 5 and verticalMotorValue <= 12.5:
-          verticalMotorValue += 0.5
-          servoVertical.ChangeDutyCycle(verticalMotorValue)
-          sleep(0.3)
-          print('up')
-          break
-
-termios.tcsetattr(sys.stdin, termios.TCSADRAIN,filedescriptors)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5030, host='192.168.1.9',threaded=True)
+    app.run(debug=True, port=5030, host='0.0.0.0',threaded=True)
